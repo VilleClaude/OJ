f7b283217c325cc626762c39745d71ef725738d6
VilleClaude <villeclaude23@gmail.com>
2024-06-12 21:36:23 +0700

    code https://marisaoj.com/problem/140

---
+++ MarisaOJ/DP/140.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define For(i,a,b) for(int i = a; i <= b; i++)
+#define Ford(i,a,b) for(int i = a; i >= b; i--)
+#define ll long long
+#define ii pair<int,int>
+#define fi first
+#define se second
+#define all(v) v.begin(),v.end()
+#define RRH(v) v.resize(unique(all(v)) - v.begin())
+
+using namespace std;
+const int  N = 1e6+7;
+const int M = 1e9+7;
+const ll oo = 3e18;
+
+int n;
+ll dp[N];
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    #define TASK ""
+    if (fopen (".inp", "r")) {
+        freopen (".inp", "r", stdin);
+        freopen (".out", "w", stdout);
+    }
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+
+    cin >> n;
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = 0;
+        for (int j = max(0, i - 3); j < i; j++) {
+            dp[i] = dp[i] + dp[j];
+            dp[i] %= M;
+        }
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
+
+
