724e8e5af879b52a08d94ab8923e67ec7ba0df8a
VilleClaude <villeclaude23@gmail.com>
2024-06-25 09:07:10 +0700

    code https://marisaoj.com/problem/131

---
+++ MarisaOJ/Data-Structure/131.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#define For(i,a,b) for(int i = a; i <= b; i++)
+#define Ford(i,a,b) for(int i = a; i >= b; i--)
+#define ll long long
+#define ii pair<int,int>
+#define fi first
+#define se second
+#define all(v) v.begin(),v.end()
+#define RRH(v) v.resize(unique(all(v)) - v.begin())
+
+using namespace std;
+const int  N = 1e6+7;
+const int M = 1e9+7;
+const ll oo = 3e18;
+
+int n, m;
+int p[N];
+ll sz[N];
+int Find(int u) {
+    if (u == p[u]) return u;
+    p[u] = Find(p[u]);
+    return p[u];
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    #define TASK ""
+    if (fopen (".inp", "r")) {
+        freopen (".inp", "r", stdin);
+        freopen (".out", "w", stdout);
+    }
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+
+    cin >> n >> m;
+    For (i, 1, n) p[i] = i, sz[i] = i;
+
+    while (m--) {
+        int t, u, v; cin >> t;
+        if (t == 1) {
+            cin >> u >> v;
+            if (u == v) continue;;
+            u = Find(u);
+            v = Find(v);
+            if (u == v) continue;;
+            if (u > v) swap(u, v);
+            p[v] = u;
+            sz[u] += sz[v];
+        }
+        else {
+            cin >> u;
+            u = Find(u);
+            cout << sz[u] << '\n';
+        }
+    }
+
+    return 0;
+}
+
+
